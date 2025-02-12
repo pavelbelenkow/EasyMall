8194ed4d25a55b7573f0aca9f0aa88f368b72d66
Pavel Belenkow <heartofcassiopeia@yandex.ru>
2025-02-12 18:32:36 +0300

    Initial Commit

---
+++ EasyMall/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  EasyMall
+//
+//  Created by Pavel Belenkow on 12.02.2025.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
